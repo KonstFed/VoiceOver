bf6779cf419814a74a747d5e3410d2af7a50e181
nbogdankov@gmail.com <nbogdankov@gmail.com>
2023-10-12 17:40:39 +0300

    header improved

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from './Header.module.scss'
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+    const location = useLocation();
+    const isTextToSpeechActive = location.pathname === '/text-to-speech';
+    const isSpeechToSpeechActive = location.pathname === '/speech-to-speech';
+
     return (
         <div className={styles.header}>
             <div className={styles.menu}>
-                <a className={styles.menu_item} href="/text-to-speech">Text to Speech</a>
-                <a className={styles.menu_item} href="/speech-to-speech">Speech to Speech</a>
+                <a className={`${styles.menu_item} ${isTextToSpeechActive ? styles.active : ''}`} href="/text-to-speech">Text to Speech</a>
+                <a className={`${styles.menu_item} ${isSpeechToSpeechActive ? styles.active : ''}`} href="/speech-to-speech">Speech to Speech</a>
             </div>
         </div>
     )
 }
 
-export default Header
+export default Header;
